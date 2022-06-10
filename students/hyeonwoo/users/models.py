36426e667768665d79d638168a4659c5d468a264
HyeonWooJo <HyeonWooJo@github.com>
2022-06-10 17:36:22 +0900

    Fix: models.py

--- students/hyeonwoo/users/models.py
+++ students/hyeonwoo/users/models.py
@@ -2,10 +2,12 @@ from decimal import MAX_EMAX
 from django.db import models
 
 class User(models.Model):
-    name               = models.CharField(max_length=45)
-    email              = models.CharField(max_length=45)
-    password           = models.CharField(max_length=45)
-    mobilephone_number = models.CharField(max_length=45)
+    first_name    = models.CharField(max_length=45)
+    last_name     = models.CharField(max_length=45)
+    user_name     = models.CharField(max_length=45)
+    email         = models.EmailField(max_length=45, unique=True)
+    password      = models.CharField(max_length=45)
+    mobile_number = models.CharField(max_length=45)
 
     class Meta:
         db_table = "users"
