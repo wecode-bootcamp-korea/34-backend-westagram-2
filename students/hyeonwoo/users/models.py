4fdfd1d48f12ee463274d554b1f33f81bfbe5087
HyeonWooJo <HyeonWooJo@github.com>
2022-06-10 16:44:05 +0900

    Add: startapp users / Fix: models.py

--- students/hyeonwoo/users/models.py
+++ students/hyeonwoo/users/models.py
@@ -1,8 +1,11 @@
 from decimal import MAX_EMAX
 from django.db import models
 
-class UserInfo(models.Model):
+class User(models.Model):
     name               = models.CharField(max_length=45)
     email              = models.CharField(max_length=45)
     password           = models.CharField(max_length=45)
     mobilephone_number = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = "users"
