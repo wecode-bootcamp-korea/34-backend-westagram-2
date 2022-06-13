42091f1a7b07220f5c084245272c1af7e44425aa
KOCEAN <kocean@KOCEANui-MacBookPro.local>
2022-06-13 14:39:39 +0900

    User Data models fixed

--- students/juyeob/westagram/users/models.py
+++ students/juyeob/westagram/users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.EmailField(max_length=45)
-    password = models.CharField(max_length=45)
-    phone = models.CharField(max_length=45)
+    first_name    = models.CharField(max_length=45)
+    last_name     = models.CharField(max_length=45)
+    email         = models.EmailField(max_length=45, unique=True)
+    password      = models.CharField(max_length=45)
+    phone_number  = models.CharField(max_length=45)
+    
     class Meta:
         db_table = 'users'
