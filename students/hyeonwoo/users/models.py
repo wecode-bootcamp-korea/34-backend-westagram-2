a23f881e06cdf55cb42933c945c798ebcba008fa
HyeonWooJo <HyeonWooJo@github.com>
2022-06-14 11:37:27 +0900

    Fix: models.py

--- students/hyeonwoo/users/models.py
+++ students/hyeonwoo/users/models.py
@@ -2,12 +2,12 @@ from decimal import MAX_EMAX
 from django.db import models
 
 class User(models.Model):
-    first_name    = models.CharField(max_length=45, blank=False)
-    last_name     = models.CharField(max_length=45, blank=False)
-    user_name     = models.CharField(max_length=45, unique=True, blank=False)
-    email         = models.EmailField(max_length=45, unique=True, blank=False)
-    password      = models.CharField(max_length=45 , blank=False)
-    mobile_number = models.CharField(max_length=45, blank=False)
+    first_name    = models.CharField(max_length=45)
+    last_name     = models.CharField(max_length=45)
+    user_name     = models.CharField(max_length=45, unique=True)
+    email         = models.EmailField(max_length=45, unique=True)
+    password      = models.CharField(max_length=45)
+    mobile_number = models.CharField(max_length=45)
 
     class Meta:
         db_table = "users"
--- students/hyeonwoo/users/urls.py
+++ students/hyeonwoo/users/urls.py
@@ -1,5 +1,6 @@
+from distutils.log import Log
 from django.urls import path
-from users.views import SignUp
+from users.views import SignUp, LogIn
 
 urlpatterns = [
     path('/signup', SignUp.as_view()),
